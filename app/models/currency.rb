8c497e7f49a6bc9aa108f7dab53c52aaa366a910
Roy Wei <roywjy@gmail.com>
2025-08-03 14:15:22 +1000

    Refactor Currency model and tests

--- app/models/currency.rb
+++ app/models/currency.rb
@@ -1,29 +1,17 @@
 class Currency < ApplicationRecord
-  # Associations
   has_many :price_snapshots, dependent: :destroy
   
-  # Validations
   validates :name, presence: true, uniqueness: true
+  # Validate symbol is 3-10 uppercase letters
   validates :symbol, presence: true, uniqueness: true, format: { with: /\A[A-Z]{3,10}\z/ }
   
-  # Scopes
   scope :ordered, -> { order(:name) }
   
-  # Instance methods
   def latest_price
     price_snapshots.order(captured_at: :desc).first
   end
   
   def latest_price_value
     latest_price&.last
-  end
-  
-  # Class methods
-  def self.btc
-    find_by(symbol: 'BTCAUD')
-  end
-  
-  def self.eth
-    find_by(symbol: 'ETHAUD')
   end
 end
